9f3bd057945112fd82ef515fa390572ee0c920fd
Yazan Amer <yazan.amer@progressoft.com>
2023-07-15 13:11:31 +0300

    basic client...

--- src/frontend/client.js
+++ src/frontend/client.js
@@ -14,6 +14,61 @@ function getHeaderContent() {
   xhr.send();
 }
 
+function submitRequest() {
+  const form = document.getElementById("problem-form-form");
+  if (form.checkValidity()) {
+    console.log("Submitted!");
+    console.log(document.getElementById("user-height-input").value);
+    console.log(document.getElementById("camera-height-input").value);
+    console.log(form.elements["seedBike"].value);  
+  }
+}
+
+function restCall() {
+  console.log(thing);
+  let url = "http://localhost:5000/optimize";
+  const requestBody = {
+    "seed-bike": {
+      seat_x: -9,
+      seat_y: 27,
+      handle_bar_x: 16.5,
+      handle_bar_y: 25.5,
+      crank_length: 7,
+    },
+    "body-dimensions": {
+      height: 75,
+      sh_height: 61.09855828510818,
+      hip_to_ankle: 31.167514055725047,
+      hip_to_knee: 15.196207871637029,
+      shoulder_to_wrist: 13.538605228960089,
+      arm_len: 16.538605228960087,
+      tor_len: 26.931044229383136,
+      low_leg: 18.971306184088018,
+      up_leg: 15.196207871637029,
+    },
+  };
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  })
+    .then((response) => {
+      if (response.ok) {
+        let resText = response.text();
+        document.getElementById("optimizationResponse").innerText = resText;
+        return resText;
+      }
+      throw new Error("Request failed with status: " + response.status);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+}
+
 function optimizeBike() {
   console.log(thing);
   let url = "http://localhost:5000/optimize";
