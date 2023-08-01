2bdb672311fd8608958bfc3fc76c113a37408ad2
Yazan Amer <yazan.amer@progressoft.com>
2023-08-01 11:20:05 +0300

    combine files...

--- backend/src/fit_analysis/vectorizedangles.py
+++ backend/src/fit_analysis/vectorizedangles.py
@@ -1,6 +1,26 @@
 import numpy as np
 from scipy.stats import norm
-from fit_analysis.usecases import USE_DICT
+
+USE_DICT = {
+    "road": {
+        "opt_knee_angle": (37.5, 10),
+        "opt_back_angle": (45, 5),
+        "opt_awrist_angle": (90, 5),
+        "opt_ankle_angle": (100.0, 5.0),
+    },
+    "mtb": {
+        "opt_knee_angle": (37.5, 10),
+        "opt_back_angle": (50, 5),
+        "opt_awrist_angle": (90, 5),
+        "opt_ankle_angle": (100.0, 5.0),
+    },
+    "commute": {
+        "opt_knee_angle": (37.5, 10),
+        "opt_back_angle": (52, 5),
+        "opt_awrist_angle": (85, 5),
+        "opt_ankle_angle": (100.0, 5.0),
+    },
+}
 
 
 # #####
