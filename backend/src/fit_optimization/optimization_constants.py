8301fa4f0eb900ed11bc3a4ccd8b154a5bc93827
Yazan Amer <yazan.amer@progressoft.com>
2023-08-01 19:11:46 +0300

    optimization_constants.py -- fixed datatypes

--- backend/src/fit_optimization/optimization_constants.py
+++ backend/src/fit_optimization/optimization_constants.py
@@ -1,6 +1,8 @@
+from typing import List
+
 import pandas as pd
 from decode_mcd import ContinuousTarget, DesignTargets
-from pymoo.core.variable import Real, Choice
+from pymoo.core.variable import Real, Choice, Variable
 
 from fit_optimization.bike_dataset_builder import build_performances, get_bikes
 
@@ -11,12 +13,13 @@ BACK_TARGET = ContinuousTarget(label='Armpit Angle', lower_bound=5, upper_bound=
 KNEE_TARGET = ContinuousTarget(label='Knee Extension', lower_bound=10, upper_bound=37.5)
 TARGETS = DesignTargets([KNEE_TARGET, BACK_TARGET, AMPIT_WRIST_TARGET, ])
 
-FEATURES_DATATYPES = [Real(bounds=(-1000, 1000)) for _ in range(13)] + [Choice(options=(0, 1, 2))]
-
 PERFORMANCES = pd.DataFrame.from_records(build_performances())
 
 all_bikes = get_bikes()
 DESIGNS = pd.DataFrame.from_records(all_bikes)
+FEATURES_DATATYPES: List[Variable]
+FEATURES_DATATYPES = [Real(bounds=(DESIGNS.iloc[:, _].min(), DESIGNS.iloc[:, _].max())) for _ in range(13)]
+FEATURES_DATATYPES.append(Choice(options=DESIGNS.iloc[:, 13].unique()))
 
 
 def build_seed_map():
