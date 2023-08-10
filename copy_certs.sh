92c229cc490f3c7c12bae1e6ded750ef7a0c228a
Yazan Amer <yazan.amer@progressoft.com>
2023-08-10 09:30:35 +0300

    nginx configured to use real certs

---
+++ copy_certs.sh
@@ -0,0 +1,3 @@
+cp /etc/letsencrypt/live/mcd-demo.com/fullchain.pem nginx/secrets/fullchain.pem
+cp /etc/letsencrypt/live/mcd-demo.com/privkey.pem nginx/secrets/privkey.pem
+#sudo openssl dhparam -out dhparam.pem 4096
