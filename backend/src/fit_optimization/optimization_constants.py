af7c3ecfda89a2bff5d6733ba1d8968d8cabea2e
Yazan Amer <yazan.amer@progressoft.com>
2023-08-14 18:53:24 +0300

    flipped var names

--- backend/src/fit_optimization/optimization_constants.py
+++ backend/src/fit_optimization/optimization_constants.py
@@ -18,9 +18,9 @@ def validate_seat_height(bike_df) -> pd.DataFrame:
     return results.astype("int32")
 
 
-AMPIT_WRIST_TARGET = ContinuousTarget(label='Back Angle', lower_bound=5, upper_bound=45)
+BACK_TARGET = ContinuousTarget(label='Back Angle', lower_bound=5, upper_bound=45)
 
-BACK_TARGET = ContinuousTarget(label='Armpit Angle', lower_bound=5, upper_bound=90)
+AMPIT_WRIST_TARGET = ContinuousTarget(label='Armpit Angle', lower_bound=5, upper_bound=90)
 
 KNEE_TARGET = ContinuousTarget(label='Knee Extension', lower_bound=10, upper_bound=37.5)
 TARGETS = DesignTargets([KNEE_TARGET, BACK_TARGET, AMPIT_WRIST_TARGET, ])
