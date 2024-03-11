1e20cfc35afa83acc9879fb868f3ff020dd8cfe8
Yazan Amer <yazan.amer@progressoft.com>
2024-03-11 16:38:22 +0300

    fix: copy_certs.sh

--- copy_certs.sh
+++ copy_certs.sh
@@ -1,4 +1,4 @@
 mkdir nginx/secrets -p
-cp /etc/letsencrypt/live/mcd-demo.com/fullchain.pem nginx/secrets/fullchain.pem
-cp /etc/letsencrypt/live/mcd-demo.com/privkey.pem nginx/secrets/privkey.pem
+cp /etc/letsencrypt/live/mcd-demo.net/fullchain.pem nginx/secrets/fullchain.pem
+cp /etc/letsencrypt/live/mcd-demo.net/privkey.pem nginx/secrets/privkey.pem
 #sudo openssl dhparam -out dhparam.pem 4096
