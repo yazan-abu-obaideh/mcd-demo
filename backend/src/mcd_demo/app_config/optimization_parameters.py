3d377d15c59ba3bb3abd23537a7873a5dfab21f5
Yazan Amer <yazan.amer@protonmail.com>
2024-06-15 10:18:16 +0200

    feat: text prompt generation

--- backend/src/mcd_demo/app_config/optimization_parameters.py
+++ backend/src/mcd_demo/app_config/optimization_parameters.py
@@ -1,4 +1,4 @@
 from mcd_demo.app_config._config_utils import get_config
 
-OPTIMIZER_GENERATIONS = int(get_config("OPTIMIZER_GENERATIONS", 25))
+OPTIMIZER_GENERATIONS = int(get_config("OPTIMIZER_GENERATIONS", 55))
 OPTIMIZER_POPULATION = int(get_config("OPTIMIZER_POPULATION", 85))
