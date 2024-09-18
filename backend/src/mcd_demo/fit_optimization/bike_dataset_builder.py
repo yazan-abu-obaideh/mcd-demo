8088593a6356408abae6b8256e66bf5f87e7ef21
Yazan Amer <yazan.amer@protonmail.com>
2024-09-18 13:19:42 +0200

    chore: remove unused

--- backend/src/mcd_demo/fit_optimization/bike_dataset_builder.py
+++ backend/src/mcd_demo/fit_optimization/bike_dataset_builder.py
@@ -5,12 +5,6 @@ import pandas as pd
 
 from mcd_demo.fit_analysis.demoanalysis_wrapped import calculate_angles, calculate_drag
 
-BODY_DIMENSIONS = {'height': 75, 'sh_height': 61.09855828510818, 'hip_to_ankle': 31.167514055725047,
-                   'hip_to_knee': 15.196207871637029, 'shoulder_to_wrist': 13.538605228960089,
-                   'arm_len': 16.538605228960087,
-                   'tor_len': 26.931044229383136, 'low_leg': 18.971306184088018,
-                   'up_leg': 15.196207871637029}
-
 
 def get_bikes():
     csv_path = os.path.join(os.path.dirname(__file__), "../resources/bike_vector_df_with_id.csv")
