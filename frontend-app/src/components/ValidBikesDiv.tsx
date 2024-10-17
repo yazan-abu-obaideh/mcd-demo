060eaabc93e08a80b2b762fd6f1d6faf325835b4
Yazan Amer <yazan.amer@protonmail.com>
2024-10-17 13:23:55 +0200

    refactor forms

--- frontend-app/src/components/ValidBikesDiv.tsx
+++ frontend-app/src/components/ValidBikesDiv.tsx
@@ -3,70 +3,72 @@ import { optimizationController } from "../declarative/client";
 import { GeneratedBike } from "../declarative/controller";
 import { McdServerResponse } from "./McdServerResponse";
 
-const ACTION_BUTTON_CSS = "btn btn-outline-danger btn-lg";
 
-export function ValidBikesDiv(props: { mcdServerResponse: McdServerResponse }) {
+const urlCreator = window.URL || window.webkitURL;
+
+const ACTION_BUTTON_CSS = "btn btn-outline-danger btn-lg";
+const DOWNLOAD_FAILED = { active: false, innerText: "Download failed" };
+const DOWNLOAD_SUCCESSFUL = {
+  active: false,
+  innerText: "Downloaded successfully",
+};
+const DOWNLOADING = {
+  active: false,
+  innerText: "Downloading...",
+};
+
+type RenderingState = {
+  renderingRequest: boolean;
+};
+
+function CarouselControl(props: { controlType: "prev" | "next" }) {
   return (
-    <div id="response-received-div" className="p-3">
-      <div id="carouselExampleDark" className="carousel carousel-dark slide">
-        <div
-          id="generated-designs-consumer-carousel"
-          className="carousel-inner"
-        >
-          <div>
-            {props.mcdServerResponse.optimizationResponse?.bikes.map(
-              (b, index) => {
-                console.log("Mapping bike with index " + index);
-                const active = index === 0 ? "active" : undefined;
-                const className =
-                  "container text-center border rounded carousel-item mb-1 p-5 optimized-bike-div " +
-                  active;
-                return (
-                  <div className={className}>
-                    <h4>Generated Bike {index + 1}</h4>
-                    <h5>{b.bikePerformance}</h5>
-                    <br></br>
-                    <BikeActionButton
-                      onClick={() => {}}
-                      innerText="Render Bike"
-                    />
-                    <div style={{ height: "5px" }}></div>
-                    {DownloadCadButton(b)}
-                  </div>
-                );
-              }
-            )}
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
+    <button
+      className={"carousel-control-" + props.controlType}
+      type="button"
+      data-bs-target="#bikesCarousel"
+      data-bs-slide={props.controlType}
+    >
+      <span
+        className={`carousel-control-${props.controlType}-icon`}
+        aria-hidden="true"
+      ></span>
+    </button>
+  );
+}
+
+function BikeElement(props: {
+  index: number;
+  bike: { bike: GeneratedBike; bikePerformance: string };
+}) {
+  const [renderingState, setRenderingState] = useState({
+    renderingRequested: false,
+    renderingResult: undefined,
+  });
+
+  const active = props.index === 0 ? "active" : undefined;
+  const className =
+    "container text-center border rounded carousel-item mb-1 p-5 optimized-bike-div " +
+    active;
+
+  return (
+    <div className={className}>
+      <h4>Generated Bike {props.index + 1}</h4>
+      <h5>{props.bike.bikePerformance}</h5>
+      <br></br>
+      <button onClick={() => {}} className={ACTION_BUTTON_CSS}>
+        Render Bike
+      </button>
+      <div style={{ height: "5px" }}></div>
+      <DownloadCadButton
+        bike={props.bike.bike}
+        bikePerformance={props.bike.bikePerformance}
+      />
     </div>
   );
 }
-function DownloadCadButton(b: {
+
+function DownloadCadButton(props: {
   bike: GeneratedBike;
   bikePerformance: string;
 }) {
@@ -74,44 +76,67 @@ function DownloadCadButton(b: {
     active: true,
     innerText: "Download CAD",
   });
-
-  const extraClass = buttonState.active ? " " : " disabled";
   return (
     <button
-      className={ACTION_BUTTON_CSS + extraClass}
+      className={ACTION_BUTTON_CSS + (buttonState.active ? " " : " disabled")}
       onClick={() => {
-        b.bike.seedImageId = "1";
+        setButtonState(DOWNLOADING);
         const req = new GeneratedBike();
-        req.bikeObject = b.bike;
+        req.bikeObject = props.bike;
         // TODO: req.seedImageId = "1";
         optimizationController
           .postDownloadBikeCadRequest(req)
           .then((res) => {
             if (res.status === 200) {
               res.text().then((resText) => {
-                const anchor = document.createElement("a");
-                anchor.setAttribute("download", "bike.xml");
-                anchor.setAttribute(
-                  "href",
-                  "data:application/xml;charset=utf-8," +
-                    encodeURIComponent(resText)
-                );
-                anchor.click();
-                anchor.remove();
-                setButtonState({
-                  active: false,
-                  innerText: "Download successful",
-                });
+                downloadTextFile(resText);
+                setButtonState(DOWNLOAD_SUCCESSFUL);
               });
+            } else {
+              setButtonState(DOWNLOAD_FAILED);
             }
           })
-          .catch((err) => {
-            setButtonState({ active: false, innerText: "Download failed" });
+          .catch(() => {
+            setButtonState(DOWNLOAD_FAILED);
           });
       }}
     >
       {buttonState.innerText}
     </button>
+  );
+}
+
+function downloadTextFile(resText: string) {
+  const anchor = document.createElement("a");
+  anchor.setAttribute("download", "bike.bcad");
+  anchor.setAttribute(
+    "href",
+    "data:application/xml;charset=utf-8," + encodeURIComponent(resText)
+  );
+  anchor.click();
+  anchor.remove();
+}
+
+export function ValidBikesDiv(props: { mcdServerResponse: McdServerResponse }) {
+  return (
+    <div id="response-received-div" className="p-3">
+      <div id="bikesCarousel" className="carousel carousel-dark slide">
+        <div
+          id="generated-designs-consumer-carousel"
+          className="carousel-inner"
+        >
+          <div>
+            {props.mcdServerResponse.optimizationResponse?.bikes.map(
+              (bike, index) => {
+                return <BikeElement bike={bike} index={index} />;
+              }
+            )}
+          </div>
+        </div>
+        <CarouselControl controlType="prev" />
+        <CarouselControl controlType="next" />
+      </div>
+    </div>
   );
 }
 
